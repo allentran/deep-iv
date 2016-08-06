e7b8d5ac898cc3a5a53d5dd8b210a537b4b71469
Allen Tran <realallentran@gmail.com>
2016-08-05 23:05:46 -0700

    separate model to find good instruments

--- deepiv/model/feedforward.py
+++ deepiv/model/feedforward.py
@@ -1,7 +1,13 @@
+import logging
 
-from lasagne import layers, nonlinearities, updates
+import numpy as np
+from sklearn.cross_validation import train_test_split
+from lasagne import layers, nonlinearities, updates, objectives
 import theano.tensor as TT
 import theano
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 def gmm_loss(predicted, targets, instruments):
@@ -15,14 +21,59 @@ class FeedForwardModel(object):
 
     def __init__(self, n_vars, n_instruments, **kwargs):
 
-        self.n_vars = n_vars
-        self.n_instruments = n_instruments
+        self.build_instrument_model(n_vars - 1 + n_instruments, **kwargs)
+        self.build_treatment_model(n_vars, **kwargs)
+
+    def build_instrument_model(self, n_vars, **kwargs):
+
+        targets = TT.vector()
+        instrument_vars = TT.matrix()
+
+        instruments = layers.InputLayer((None, n_vars), instrument_vars)
+
+        dense_layer = layers.DenseLayer(instruments, kwargs['dense_size'], nonlinearity=nonlinearities.leaky_rectify)
+        dense_layer = layers.batch_norm(dense_layer)
+
+        for _ in xrange(kwargs['n_dense_layers'] - 1):
+            dense_layer = layers.DenseLayer(dense_layer, kwargs['dense_size'], nonlinearity=nonlinearities.leaky_rectify)
+            dense_layer = layers.batch_norm(dense_layer)
+
+        output = layers.DenseLayer(dense_layer, 1, nonlinearity=nonlinearities.sigmoid)
+        prediction = layers.get_output(output)
+
+        # flexible here, endog variable can be categorical, continuous, etc.
+        loss = objectives.binary_crossentropy(prediction.flatten(), targets.flatten()).mean()
+        loss_total = objectives.binary_crossentropy(prediction.flatten(), targets.flatten()).sum()
+
+        params = layers.get_all_params(output, trainable=True)
+        param_updates = updates.adadelta(loss, params)
+
+        self._instrument_train_fn = theano.function(
+            [
+                targets,
+                instrument_vars,
+            ],
+            loss,
+            updates=param_updates
+        )
+
+        self._instrument_loss_fn = theano.function(
+            [
+                targets,
+                instrument_vars,
+            ],
+            loss_total
+        )
+
+        self._instrument_output_fn = theano.function([instrument_vars], prediction)
+
+    def build_treatment_model(self, n_vars, **kwargs):
 
         input_vars = TT.matrix()
         instrument_vars = TT.matrix()
         targets = TT.vector()
 
-        inputs = layers.InputLayer((None, self.n_vars), input_vars)
+        inputs = layers.InputLayer((None, n_vars), input_vars)
 
         dense_layer = layers.DenseLayer(inputs, kwargs['dense_size'], nonlinearity=nonlinearities.leaky_rectify)
         dense_layer = layers.batch_norm(dense_layer)
@@ -49,22 +100,96 @@ class FeedForwardModel(object):
             updates=param_updates
         )
 
-        self._debug_fn = theano.function(
+        self._loss_fn = theano.function(
             [
                 input_vars,
                 targets,
-                instrument_vars
+                instrument_vars,
             ],
-            loss,
+            updates=param_updates
         )
 
+        self._output_fn = theano.function(
+            [
+                input_vars,
+            ],
+            prediction,
+        )
 
-    def fit(self, x, z, y):
+    def fit(self, x, z, y, **kwargs):
+
+        def split_data(X, Z, y, test=0.1):
+            return train_test_split(X, Z, y, test_size=test, stratify=X[:, -1])
+
+        TOL = 5
+
         x = x.astype('float32')
         y = y.astype('float32')
         z = z.astype('float32')
+
+        x_train, x_test, z_train, z_test, y_train, y_test  = split_data(x, z, y)
+
+        epochs_since_smallest = -1
+        smallest_test_loss = 1e10
+        epochs = 0
+        while epochs_since_smallest < TOL:
+            epochs += 1
+            training_loss = 0
+            for batch in iterate_minibatches(x_train, y_train, z_train, batchsize=kwargs['batchsize'], shuffle=True):
+                x_b, y_b, z_b = batch
+                training_loss += self._instrument_train_fn(x_b[:, -1], z_b)
+
+            test_loss = self._instrument_loss_fn(x_test[:, -1], z_test)
+            logger.debug("train loss: %.2f, test loss: %.2f, epochs=%s", training_loss, test_loss, epochs)
+            if test_loss < smallest_test_loss:
+                smallest_test_loss = test_loss
+                epochs_since_smallest = 0
+            else:
+                epochs_since_smallest += 1
+
+        # fit model on whole dataset
+        for _ in xrange(epochs):
+            training_loss = 0
+            for batch in iterate_minibatches(x, y, z, batchsize=kwargs['batchsize'], shuffle=True):
+                x_b, y_b, z_b = batch
+                training_loss += self._instrument_train_fn(x_b[:, -1], z_b)
+
+        logger.info("instrument loss: %.2f, epochs=%s", training_loss / x.shape[0], epochs)
+
+        zhat = self._instrument_output_fn(z)
+
+        logger.info("corr(treat, instrument): %.2f", np.corrcoef(zhat[:, 0], x[:, -1])[0, 1])
+
         for _ in xrange(30):
-            loss = self._train_fn(x, y, z)
+            self._train_fn(x, y, zhat)
+
+    def get_treatment_effect(self, x):
+
+        x = x.astype('float32')
+
+        new_x = x.copy()
+        new_x[:, -1] = 1 - new_x[:, -1]
+
+        yhat = self._output_fn(x)
+        flipped = self._output_fn(new_x)
+
+        treated_mask = x[:, -1] == 1
+        treatment_effect_treated = yhat[treated_mask, :] - flipped[treated_mask, :]
+        treatment_effect_not = flipped[~treated_mask, :] - yhat[~treated_mask, :]
+        treatment_effect = (1. / x.shape[0]) * (treatment_effect_not.sum() + treatment_effect_treated.sum())
+
+        return treatment_effect
 
 
+def iterate_minibatches(inputs, targets, instruments, batchsize, shuffle=False):
+    assert len(inputs) == len(targets) == len(instruments)
+    if shuffle:
+        indices = np.arange(len(inputs))
+        np.random.shuffle(indices)
+    for start_idx in range(0, len(inputs) - batchsize + 1, batchsize):
+        if shuffle:
+            excerpt = indices[start_idx:start_idx + batchsize]
+        else:
+            excerpt = slice(start_idx, start_idx + batchsize)
+        yield inputs[excerpt], targets[excerpt], instruments[excerpt]
 
--- paper/experiments.py
+++ paper/experiments.py
@@ -3,6 +3,8 @@ import logging
 import pandas as pd
 import numpy as np
 
+from deepiv.model import feedforward
+
 RANDOM_SEED = 1692
 
 logging.basicConfig(format='%(asctime)s %(message)s', level=logging.INFO)
@@ -13,15 +15,21 @@ class TreatmentEffectGenerator(object):
 
     def __init__(self, path='paper/data/ihdp.csv'):
 
-        self.df = pd.read_csv(path)
-        np.random.seed(1692)
+        self.df = pd.read_csv(path).drop('Unnamed: 0', axis=1)
 
-    def generate_treatment_effects(self, treatment_effect=100.):
+    def simulate_data(self, sine):
+
+        self._generate_instruments(sine)
+        self._generate_treatment_effects(1.)
+
+        return self.df
+
+    def _generate_treatment_effects(self, treatment_effect=100.):
 
         error = 0.2 * np.random.randn(len(self.df)) + 0.5 * treatment_effect * self.df['momwhite']
-        self.df['treatment_effect'] = treatment_effect * self.df['treatment'] + error
+        self.df['treatment_effect'] = treatment_effect * self.df['new_treat'] + error
 
-    def generate_instruments(self, sine=False):
+    def _generate_instruments(self, sine=False):
 
         # TODO: try + cos() if momblack=1 for embeddings
 
@@ -38,8 +46,58 @@ class TreatmentEffectGenerator(object):
 
         logger.info('Correlation between instrument and treatment: %.2f', self.df[['instrument', 'new_treat']].corr().values[0, 1])
 
+
+class EstimationExperiments(object):
+
+    def __init__(self):
+
+        self.treatment_gen = TreatmentEffectGenerator()
+
+        self.x, self.mean_x, self.std_x = self.get_fixed_x()
+
+    def get_fixed_x(self):
+
+        fixed_x_cols = [
+            'bw',
+            'b.head',
+            'preterm',
+            'birth.o',
+            'nnhealth',
+            'momage',
+            'sex',
+            'twin',
+            'b.marr',
+            'mom.lths',
+            'mom.hs',
+            'mom.scoll',
+            'cig',
+            'first',
+            'booze',
+            'drugs',
+            'work.dur',
+            'prenatal'
+        ]
+
+        x = self.treatment_gen.df[fixed_x_cols].values
+        mean = np.mean(x, axis=0, keepdims=True)
+        std = np.std(x, axis=0, keepdims=True)
+        return (x - mean) / std, mean, std
+
+
+    def estimate(self):
+
+
+        np.random.seed(1692)
+        for _ in xrange(20):
+            model = feedforward.FeedForwardModel(19, 1, dense_size=10, n_dense_layers=5)
+            df = self.treatment_gen.simulate_data(True)
+            X = np.hstack((self.x, df['new_treat'].values[:, None]))
+            Z = np.hstack((self.x, df['instrument'].values[:, None]))
+            model.fit(X, Z, df['treatment_effect'].values, batchsize=128)
+            model.get_treatment_effect(X)
+
 if __name__ == "__main__":
 
-    tt = TreatmentEffectGenerator()
-    tt.generate_instruments()
+    estimates = EstimationExperiments()
+    estimates.estimate()
 
